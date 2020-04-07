39cfa99cc19d5901f703a18dfa7643fffbeffd18
Mark Mann <mann.brinson@gmail.com>
2020-04-07 15:30:50 -0700

    test model functionality added

--- neural_networks/neural_networks.py
+++ neural_networks/neural_networks.py
@@ -3,9 +3,12 @@ from random import seed
 import random
 from PIL import Image
 
+import numpy as np #For visualization
+import matplotlib.pyplot as plt #For visualization
+
 def normalize_dataset(file):
 	'''Iterate through each .pgm file in list to create list of pixels. 
-	input: file - containing list of filenames (i.e. downgesture_train.list.txt
+	input: file - containing list of filenames (i.e. downgesture_train.list.txt)
 	output: dataset - list of lists
 	'''
 	with open(file, 'r') as train_data:
@@ -76,7 +79,11 @@ def feed_forward(network, inputs):
 			sum_wx = summation(perceptron['weights'], inputs)
 			perceptron['output'] = sigmoid(sum_wx) #Update the output of each perceptron
 			new_inputs.append(perceptron['output'])
+
+		#after all perceptrons finish outputting
+		#set inputs to new_inputs to prepare for next layer
 		inputs = new_inputs
+
 	return inputs #only outputs of the last layer are returned
 
 def backward_propagate_error(network, expected):
@@ -125,6 +132,7 @@ def train_network(network, dataset, l_rate, n_epoch, n_outputs):
 	'''Given a # of epochs, train a network example-by-example. 
 	After each image example, calculate error and back propagate error recursively 
 	through the pre-existing weights.'''
+	sum_errors = []
 	for epoch in range(n_epoch):
 		sum_error = 0
 		for row in dataset:
@@ -137,22 +145,120 @@ def train_network(network, dataset, l_rate, n_epoch, n_outputs):
 			backward_propagate_error(network, expected)
 			update_weights(network, row, l_rate)
 		print('>epoch=%d, error=%.3f' % (epoch, sum_error))
+		sum_errors.append(sum_error)
+	sum_errors = enumerate(sum_errors, 1)
+	return sum_errors
 
 def predict(network, row):
+	'''Get the outputs of the last layer and tell us which class index ranked higher. '''
 	outputs = feed_forward(network, row)
 	return outputs.index(max(outputs))
 
-dataset = normalize_dataset('downgesture_train.list.txt')
-n_inputs = len(dataset[0]) - 1
-n_outputs = len(set([row[-1] for row in dataset])) #number of unique classes
-#n_outputs = 2
-n_hidden = 100
-learning_rate = 0.1
-epochs = 10
-network = initialize_network(n_inputs, n_hidden, n_outputs)
-train_network(network, dataset, learning_rate, epochs, n_outputs)
-for layer in network:
-	print(layer)
-for row in dataset:
-	prediction = predict(network, row)
-	print('Expected=%d, Got=%d' % (row[-1], prediction))
+def build_train_model(n_hidden, l_rate, epochs):
+	train_data = normalize_dataset('downgesture_train.list.txt')
+	n_inputs = len(train_data[0]) - 1
+	n_outputs = len(set([row[-1] for row in train_data])) #number of unique classes
+	# n_hidden = 10
+	# learning_rate = 0.1
+	# epochs = 10
+	network = initialize_network(n_inputs, n_hidden, n_outputs)
+	sum_errors = train_network(network, train_data, l_rate, epochs, n_outputs)
+	return network, sum_errors
+
+class test_stats():
+	def __init__(self, accuracy_table):
+		'''Build up a confusion matrix to enable calculation of 
+		accuracy, precision, and recall. Then store then in the class object 
+		for easy retreival later on.'''
+		confusion_table = []
+		sum_tn = 0
+		sum_fp = 0
+		sum_fn = 0
+		sum_tp = 0
+		total = 0
+		for outcome in accuracy_table:
+			if outcome['expected'] ==0 and outcome['got'] == 0: #True Negative
+				confusion_table.append({'expected': outcome['expected'], 'got': outcome['got'], 'tp': 0, 'tn': 1, 'fp': 0, 'fn': 0})
+				sum_tn += 1
+			elif outcome['expected'] ==0 and outcome['got'] == 1: #False Positive
+				confusion_table.append({'expected': outcome['expected'], 'got': outcome['got'], 'tp': 0, 'tn': 0, 'fp': 1, 'fn': 0})
+				sum_fp += 1
+			elif outcome['expected'] ==1 and outcome['got'] == 0: #False Negative
+				confusion_table.append({'expected': outcome['expected'], 'got': outcome['got'], 'tp': 0, 'tn': 0, 'fp': 0, 'fn': 1})
+				sum_fn += 1
+			elif outcome['expected'] ==1 and outcome['got'] == 1: #True Positive
+				confusion_table.append({'expected': outcome['expected'], 'got': outcome['got'], 'tp': 1, 'tn': 0, 'fp': 0, 'fn': 0})
+				sum_tp += 1
+			total += 1
+		try:
+			accuracy = round(((sum_tp + sum_tn) / total), 4)
+		except ZeroDivisionError:
+			accuracy = 0
+		try:
+			precision = round((sum_tp / (sum_tp + sum_fp)), 4)
+		except ZeroDivisionError:
+			precision = 0
+		try:
+			recall = round((sum_tp / (sum_tp + sum_fn)), 4)
+		except ZeroDivisionError:
+			recall = 0
+		#Save all calculations to the class
+		self.accuracy = accuracy
+		self.precision = precision
+		self.recall = recall
+		self.confusion_table = confusion_table
+		self.sum_tn = sum_tn
+		self.sum_fp = sum_fp
+		self.sum_fn = sum_fn
+		self.sum_tp = sum_tp
+		self.total = total
+
+def plot_errors(iter_errors):
+    '''Plot a y-axis graph of erros per iterations'''
+    iters = []
+    errors = []
+    for i in iter_errors:
+        iters.append(i[0])
+        errors.append(i[1])
+    iters = np.asarray(iters) #x
+    errors = np.asarray(errors) #y1
+
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('iterations')
+    ax1.set_ylabel('errors', color=color)
+    ax1.plot(iters, errors, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()
+    plt.show()
+
+def test_model():
+	#Model parameters
+	n_hidden = 20
+	l_rate = 0.1
+	epochs = 100
+	#Train a neural network 
+	network, sum_errors = build_train_model(n_hidden, l_rate, epochs)
+	#load the test dataset
+	test_data = normalize_dataset('downgesture_test.list.txt')
+	accuracy_table = []
+	for row in test_data:
+		prediction = predict(network, row)
+		#print('Expected=%d, Got=%d' % (row[-1], prediction))
+		accuracy_table.append({'expected': row[-1], 'got': prediction})
+	test_results = test_stats(accuracy_table)
+
+	print('Accuracy Table')
+	for row in accuracy_table:
+		print(row)
+
+	print('Accuracy: ', test_results.accuracy)
+	print('Precision: ', test_results.precision)
+	print('Recall: ', test_results.recall)
+
+	plot_errors(sum_errors)
+
+if __name__ == '__main__':
+	test_model()
