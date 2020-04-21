e19e9e47fc01646e8b89e7baeb7819a22c1a4e69
Mark Mann <40529252+mann-brinson@users.noreply.github.com>
2020-04-20 18:02:28 -0700

    Update scikit-nonlin.py

--- svm/scikit-nonlin.py
+++ svm/scikit-nonlin.py
@@ -4,16 +4,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics import classification_report, confusion_matrix
 
-
 def main():
-
 	data, labels = get_data('nonlinsep.txt')
 	data, labels, svc = fit_svm(data, labels)
 	plot(data, labels, svc)
 
-
 def get_data(file):
-
 	with open(file, "r") as fd:
 		rows = fd.read().splitlines()
 		data = []
@@ -22,24 +18,19 @@ def get_data(file):
 			row_list = row.split(',')
 			data.append(row_list[:-1])
 			labels.append(row_list[-1])
-
 	return data, labels
 
-
 def fit_svm(data, labels):
-
 	# Create support vector classifier
 	svc = svm.SVC(kernel='rbf', gamma=.01, C=1000)
 
 	# Train model
 	svc.fit(data, labels)
 	print(f'there are {len(svc.support_vectors_)} support vectors: \n {svc.support_vectors_}')
-
+	
 	return data, labels, svc
 
-
 def plot(data, labels, svc):
-
 	# Plot data points and color using their class
 	color = ['red' if c == '+1' else 'blue' for c in labels]
 	for point in data:
@@ -50,7 +41,6 @@ def plot(data, labels, svc):
 
 	# Plot the hyperplane
 	plt.axis("off"), plt.show()
-
 
 if __name__ == '__main__':
 	main()
