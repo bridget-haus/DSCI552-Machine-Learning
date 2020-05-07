92d6fd0a89a2a7ed017535cc2cd8a2aef2c892a7
Mark Mann <mann.brinson@gmail.com>
2020-05-07 12:14:21 -0700

    add random forest tests

---
+++ final_project/random_forest.py
@@ -0,0 +1,29 @@
+import scikit_tree
+from sklearn.ensemble import RandomForestClassifier
+
+#N_estimators tests
+n_est_list = [1, 10, 100, 500]
+for n_est in n_est_list:
+    model = RandomForestClassifier(criterion="entropy", n_estimators=n_est, random_state=0)
+    model.fit(data_train, labels_train)
+    label_pred = model.predict(data_test)
+    print(f'Number trees: {n_est}, Accuracy of model is: {accuracy_score(labels_test, label_pred)}')
+
+#Max_depth tests
+max_depth_list = [1, 2, 5, 10, 20]
+for depth in max_depth_list:
+    model = RandomForestClassifier(criterion="entropy", n_estimators=100, random_state=0, max_depth=depth)
+    model.fit(data_train, labels_train)
+    label_pred = model.predict(data_test)
+    print(f'Max depth: {depth}, Accuracy: {accuracy_score(labels_test, label_pred)}')
+
+#Max bag samples tests
+data_len = len(data_train)
+data_percents = [0.1, 0.3, 0.5, 0.8]
+for perc in data_percents:
+    max_sample = int(round(perc*data_len))
+    model = RandomForestClassifier(criterion="entropy", n_estimators=100, random_state=0, 
+                                   max_depth=5, max_samples=max_sample)
+    model.fit(data_train, labels_train)
+    label_pred = model.predict(data_test)
+    print(f'Max bag sample length: {max_sample}, Accuracy: {accuracy_score(labels_test, label_pred)}')
